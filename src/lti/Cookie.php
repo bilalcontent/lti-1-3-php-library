3158f58195de6223bf829771218ce32fb8b9ff54
Bilal Sarwar <141229622+bilalcontent@users.noreply.github.com>
2025-10-03 02:45:10 -0700

    Update Cookie.php

--- src/lti/Cookie.php
+++ src/lti/Cookie.php
@@ -12,11 +12,12 @@ class Cookie {
     public function set_cookie($name, $value, $exp = 3600, $options = []) {
         $state = \Illuminate\Support\Facades\DB::Table('lti1p3_login_state')->insert(array(
             'state' => $value,
-            'expire_at' => \Carbon\Carbon::now()->addSeconds($exp)
+            'expires_at' => \Carbon\Carbon::now()->addSeconds($exp)
         ));
     
         return $this;
     }
 }
 ?>
+
 
