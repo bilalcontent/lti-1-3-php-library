3fb57f46f7d141fb4841767e1fb9e79ec3f384ce
Bilal Sarwar <141229622+bilalcontent@users.noreply.github.com>
2025-12-01 04:50:31 -0800

    Refactor LTI_Message_Launch class structure

--- src/lti/LTI_Message_Launch.php
+++ src/lti/LTI_Message_Launch.php
@@ -283,7 +283,7 @@ class LTI_Message_Launch {
 
     private function validate_nonce() {
         if (!$this->cache->check_nonce($this->jwt['body']['nonce'])) {
-            //throw new LTI_Exception("Invalid Nonce");
+            throw new LTI_Exception("Invalid Nonce");
         }
         return $this;
     }
@@ -384,6 +384,7 @@ class LTI_Message_Launch {
     }
 }
 ?>
+
 
 
 
